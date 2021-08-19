b3ff257d9d8541e9d2ec0564bc0b23187613bbb2
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2021-08-19 13:53:01 -0300

    update CSS

--- projeto_final/index.php
+++ projeto_final/index.php
@@ -12,78 +12,87 @@ include "projeto/projetos.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Gerenciador Universidade</title>
 </head>
 
 <body>
-    <br><label>Gerenciador Universidade</label><br>
+    <div class="container">
+        <br><label id="logo">Gerenciador Universidade</label><br>
 
-    <hr>
+        <hr>
 
-    <table>
-        <label>Projetos&nbsp;</label>
-        <a href="projeto/add_projeto.php"><button>Novo Projeto</button></a>
-        <tr>
-            <td>Número</td>
-            <td>Nome</td>
-            <td>Data Inicio</td>
-            <td>Data Fim</td>
-            <td>Orçamento</td>
-            <td>Gerente</td>
-            <td></td>
-            <td></td>
-        </tr>
-        <?php projetos(); ?>
-    </table>
 
-    <hr>
+        <table class="table">
+            <label>Projetos&nbsp;</label>
+            <a href="projeto/add_projeto.php"><button class="btn btn-success">Novo Projeto</button></a>
+            <tr class="column-name">
+                <td>Número</td>
+                <td>Nome</td>
+                <td>Data Inicio</td>
+                <td>Data Fim</td>
+                <td>Orçamento</td>
+                <td>Gerente</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <?php projetos(); ?>
+        </table>
 
-    <table>
-        <label>Professores&nbsp;</label>
-        <a href="professor/add_professor.php"><button>Novo Professor</button></a>
-        <tr>
-            <td>Matricula</td>
-            <td>Nome</td>
-            <td>Sala</td>
-            <td>Especialidade</td>
-            <td></td>
-            <td></td>
-        </tr>
-        <?php professores(); ?>
-    </table>
+        <hr>
 
-    <hr>
+        <table class="table">
+            <label>Professores&nbsp;</label>
+            <a href="professor/add_professor.php"><button class="btn btn-success">Novo Professor</button></a>
+            <tr class="column-name">
+                <td>Matricula</td>
+                <td>Nome</td>
+                <td>Sala</td>
+                <td>Especialidade</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <?php professores(); ?>
+        </table>
 
-    <table>
-        <label>Estudantes&nbsp;</label>
-        <a href="estudante_pos/add_estudante.php"><button>Novo Estudante</button></a>
-        <tr>
-            <td>Matricula</td>
-            <td>Nome</td>
-            <td>Departamento</td>
-            <td></td>
-            <td></td>
-        </tr>
-        <?php estudantes(); ?>
-    </table>
+        <hr>
 
-    <hr>
+        <table class="table">
+            <label>Estudantes&nbsp;</label>
+            <a href="estudante_pos/add_estudante.php"><button class="btn btn-success">Novo Estudante</button></a>
+            <tr class="column-name">
+                <td>Matricula</td>
+                <td>Nome</td>
+                <td>Departamento</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <?php estudantes(); ?>
+        </table>
 
-    <table>
-        <label>Departamento&nbsp;</label>
-        <a href="departamento/add_departamento.php"><button>Novo Departamento</button></a>
-        <tr>
-            <td>Número</td>
-            <td>Nome</td>
-            <td>Sala</td>
-            <td>Chefe</td>
-            <td></td>
-            <td></td>
-        </tr>
-        <?php departamentos(); ?>
-    </table>
+        <hr>
 
-    <hr>
+        <table class="table">
+            <label>Departamento&nbsp;</label>
+            <a href="departamento/add_departamento.php"><button class="btn btn-success">Novo Departamento</button></a>
+            <tr class="column-name">
+                <td>Número</td>
+                <td>Nome</td>
+                <td>Sala</td>
+                <td>Chefe</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <?php departamentos(); ?>
+        </table>
+
+        <hr>
+    </div>
+
+
 </body>
 
 </html>
